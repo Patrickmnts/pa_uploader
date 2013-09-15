d40dcc7ce84449d9306a749c090af7ead816b620
Patrickmnts <patrickmnts@gmail.com>
2013-09-15 14:17:53 -0700

    add validations to upload model

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,4 +1,12 @@
 class Upload < ActiveRecord::Base
   mount_uploader :package, PackageUploader
+  validates :user_name, presence: true
+  validates :message, presence: true
+  validates :package, presence: true
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :recipient_email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX }
+  validates :user_email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX }
 end
